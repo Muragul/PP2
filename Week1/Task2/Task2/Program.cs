3f84ce2e063cd48b8dabf5bf6719b556f036cc2b
Muragul <muragulm@gmail.com>
2019-02-01 02:25:44 +0600

    added Task2

---
+++ Week1/Task2/Task2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Task2
+{
+    class Student
+    {
+        public string name;
+        public string id;
+        public int year;
+     
+        public Student(string Name, string ID, int Year)
+        {
+            this.name = Name;
+            this.id = ID;
+            this.year = Year;
+            year++;
+            Console.Write(name + " " + id + " " + year);
+        }
+       
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Name = Console.ReadLine();
+            string ID = Console.ReadLine();
+            int Year = int.Parse(Console.ReadLine());
+            Student S = new Student(Name,ID,Year);
+
+
+            Console.ReadKey();
+        }
+
+    }
+}
