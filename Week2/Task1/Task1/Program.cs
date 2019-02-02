f4788026f7c02a5e5696671e9e74d147e66f864f
Muragul <muragulm@gmail.com>
2019-02-02 11:23:16 +0600

    added Week2

---
+++ Week2/Task1/Task1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Task1
+{
+    class Program
+    {
+        public static bool Palindrome(string s)
+        {
+            for (int i = 0; i < s.Length/ 2; i++)
+            {
+                if (s[i] != s[s.Length - i - 1])
+                {
+                    return false;
+                }
+            }return true;
+        }
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            if (Palindrome(s))
+            {
+                Console.WriteLine("Yes");
+            } else
+            {
+                Console.WriteLine("No");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Week2/Task2/Task2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+
+namespace Task2
+{
+    class Program
+    {
+        public static bool isPrime(int x)
+        {
+            int cnt = 0;
+            for (int i = 2; i < x; i++)
+            {
+                if (x % i == 0) cnt++;
+            }
+            if (cnt == 0) return true;
+            return false;
+        }
+
+        static void Main(string[] args)
+        {
+            string s1 = "";
+            StreamReader SR = new StreamReader("C:/Users/Асер/Desktop/PP2/Week2/Task2/input.txt");
+            string [] arr = SR.ReadToEnd().Split();
+            SR.Close();
+            foreach (string s in arr)
+            {
+                if (isPrime(int.Parse(s)))
+                {
+                    s1 += s;
+                    s1 += " ";
+                }
+
+            }
+            StreamWriter SW = new StreamWriter("C:/Users/Асер/Desktop/PP2/Week2/Task2/output.txt");
+            SW.Write(s1);
+            SW.Close();
+
+        }
+    }
+}
