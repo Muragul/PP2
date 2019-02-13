595d434fee010a549e7c68c1c1488655815391f0
Muragul <muragulm@gmail.com>
2019-02-13 15:26:38 +0600

    changes in Task4

--- Week2/Task4/Task4/Program.cs
+++ Week2/Task4/Task4/Program.cs
@@ -1,61 +1,58 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 
 namespace Task4
 {
     class Program
     {
+        static void Create()
+        {
+            string Folder = "C:/Users/Асер/Desktop/PP2/Week2/Task4"; //путь: основная папка в которой работаем
+            string pathString1 = Path.Combine(Folder, "path"); //путь1: первая папка находится в основной
+            string pathString2 = Path.Combine(Folder, "path1"); //путь2: вторая папка находится в основной
+            string FileName = "1.txt"; //имя файла с которым будем работать
+            string sourcefile = Path.Combine(pathString1, FileName); //путь к файлу в первой папке
+            File.Create(sourcefile);//создаем файл по заданному пути
+        }
+
+        static void Copy()
+        {
+            //каждая функция работает отдельно, поэтому заново инициализируем пути
+            string Folder = "C:/Users/Асер/Desktop/PP2/Week2/Task4"; 
+            string pathString1 = Path.Combine(Folder, "path"); 
+            string pathString2 = Path.Combine(Folder, "path1");
+            string FileName = "1.txt";
+            string sourcefile = Path.Combine(pathString1, FileName);
+            string destfile = Path.Combine(pathString2, FileName);//путь к файлу во второй папке
+            //if (!File.Exists(pathString1))
+                File.Copy(sourcefile, destfile, true);
+            //если файл1 существует в первой папке, он скопируется во вторую
+        }
+
+        static void Delete()
+        {
+            string Folder = "C:/Users/Асер/Desktop/PP2/Week2/Task4"; 
+            string pathString1 = Path.Combine(Folder, "path"); 
+            string pathString2 = Path.Combine(Folder, "path1");
+            string FileName = "1.txt";
+            string sourcefile = Path.Combine(pathString1, FileName);
+            string destfile = Path.Combine(pathString2, FileName);
+            File.Delete(sourcefile);
+            //если файл1 существует в папке1, он удалится
+        }
+
         static void Main(string[] args)
         {
-            System.IO.StreamWriter textFile = new System.IO.StreamWriter("C:/Users/Асер/Desktop/PP2/Week2/Task4/path/1.txt");
-            textFile.WriteLine("Hello!");
-            textFile.Close();
+            Console.WriteLine("Choose function:");
+            Console.WriteLine("create");
+            Console.WriteLine("copy");
+            Console.WriteLine("delete");
 
-            string m = Console.ReadLine(); 
-
-            if (m == "copy")
-            {
-                string fileName = "1.txt";
-                string sourcePath = "C:/Users/Асер/Desktop/PP2/Week2/Task4/path";
-                string targetPath = "C:/Users/Асер/Desktop/PP2/Week2/Task4/path1";
-
-                string sourceFile = System.IO.Path.Combine(sourcePath, fileName);
-                string destFile = System.IO.Path.Combine(targetPath, fileName);
-
-                if (!System.IO.Directory.Exists(targetPath)) 
-                {
-                    System.IO.Directory.CreateDirectory(targetPath);
-                }
-
-
-                System.IO.File.Copy(sourceFile, destFile, true); 
-            }
-
-            string f = Console.ReadLine();
-
-            if (f == "delete")
-            {
-                if (System.IO.File.Exists("C:/Users/Асер/Desktop/PP2/Week2/Task4/path/1.txt"))
-                {
-                    try
-                    {
-                        System.IO.File.Delete("C:/Users/Асер/Desktop/PP2/Week2/Task4/path/1.txt");
-                    }
-                    catch (System.IO.IOException e)
-
-
-                    {
-                        Console.WriteLine(e.Message);
-                        return;
-                    }
-                }
-
-
-            }
-
+            string CK = Console.ReadLine();//считываем команду с консоли
+            //и вызываем необходимую функцию:
+            if (CK == "create") Create();//создание
+            if (CK == "copy") Copy();//копирование
+            if (CK == "delete") Delete();//удаление
         }
     }
 }
