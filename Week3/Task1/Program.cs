8824791182e8f07ddee59d66b74788efa806153d
Muragul <muragulm@gmail.com>
2019-02-14 03:38:35 +0600

    added comments

--- Week3/Task1/Program.cs
+++ Week3/Task1/Program.cs
@@ -3,149 +3,172 @@ using System.IO;
 
 namespace Task1
 {
+    //создаем новый класс
     class FarManager
-    {
-        public int cursor;
-        public string path;
-        public int size;
-        public bool ok;
-        DirectoryInfo directory = null;
-        FileSystemInfo currentFs = null;
+    {//с параметрами: 
+        public int cursor;//положение курсора
+        public string path;//ссылка на основную папку
+        public int size;//количество элементов в директории
+        public bool ok;//необходимость показывать скрытые папки
+        DirectoryInfo directory = null;//директорий
+        FileSystemInfo currentFs = null;//текущий файл/директорий
 
-        public FarManager()
+        public FarManager()//пустой конструктор
         {
-            cursor = 0;
+            cursor = 0;//курсор всегда стоит на начале
         }
 
-        public FarManager(string path)
+        public FarManager(string path)//конструктор: вводим путь
         {
             this.path = path;
-            cursor = 0;
-            directory = new DirectoryInfo(path);
-            size = directory.GetFileSystemInfos().Length;
-            ok = true;
+            cursor = 0;//курсор так же на нуле
+            directory = new DirectoryInfo(path);//открываем новый директорий по заданному пути
+            size = directory.GetFileSystemInfos().Length;//вычисляем количество элементов в директории
+            ok = true;//по умолчанию скрытые файлы отображаются
         }
 
-        public void Up()
+        public void Up()//функция, чтобы листать вверх
         {
-            cursor--;
-            if (cursor < 0) cursor = size - 1;
+            cursor--;//следовательно курсор будет уменьшаться
+            if (cursor < 0) cursor = size - 1;//и идти по кругу, достигнув края
         }
-        public void Down()
+        public void Down()//аналогично вниз
         {
-            cursor++;
-            if (cursor == size) cursor = 0;
+            cursor++;//курсор увеличивается
+            if (cursor == size) cursor = 0;//и вновь возвращается на первый элемент, после последнего
         }
-        public void Left()
+        public void Left()//стрелка влево
         {
-            ok = false;
+            ok = false;//скрытые файлы не отображаются
             cursor = 0;
         }
-        public void Right()
+        public void Right()//стрелка вправо
         {
-            ok = true;
-            cursor = 0;
+            ok = true;//отображаются скрытые файлы
+            cursor = 0;//курсор всегда на первом элементе
         }
 
 
-        public void Color(FileSystemInfo fs, int index)
-        {
-            if (cursor == index)
-            {
+        public void Color(FileSystemInfo fs, int index)//функция определяет окрашивание
+        {//у каждой папки/директория свой индекс
+            if (cursor == index)//если курсор совпадет с ним
+            {//выделяем
                 Console.BackgroundColor = ConsoleColor.Red;
                 Console.ForegroundColor = ConsoleColor.White;
-                currentFs = fs;
+                currentFs = fs;//и это будет текущий файл/директорий
             } else
-
-            if (fs.GetType() == typeof(DirectoryInfo))
+            //в противном случае просто определяем
+            if (fs.GetType() == typeof(DirectoryInfo))//директорий это
                 {
                     Console.BackgroundColor = ConsoleColor.Black;
                     Console.ForegroundColor = ConsoleColor.White;
                 } else
-                {
+                {//или файл
                     Console.BackgroundColor = ConsoleColor.Black;
                     Console.ForegroundColor = ConsoleColor.Yellow;
-                }
+                }//и окрашиваем в соответствующие цвета
         }
 
-        public void CalcSz()
+        public void CalcSz()//в зависимости от скрытых файлов, размер всегда будет меняться
         {
             directory = new DirectoryInfo(path);
             FileSystemInfo[] fs = directory.GetFileSystemInfos();
             size = fs.Length;
-            if (ok == false)
+            if (ok == false)//если необходимо скрывать файлы
                 for (int i = 0; i < fs.Length; i++)
-                    if (fs[i].Name[0] == '.')
-                        size--;
+                    if (fs[i].Name[0] == '.')//количество скрытых файлов (начинающихся с точки)
+                        size--;//отнимаем от размерности массива элементов
         }
 
-        public void Show()
+        public void Show()//функция будет выводить имя элемента
         {
             Console.BackgroundColor = ConsoleColor.Black;
-            Console.Clear();
+            Console.Clear();//очищаем фон
             directory = new DirectoryInfo(path);
             FileSystemInfo[] fs = directory.GetFileSystemInfos();
-            for (int i = 0, k = 0; i < fs.Length; i++)
+            for (int i = 0, k = 0; i < fs.Length; i++)//проверяем каждый элемент массива
             {
-                if (ok == false && fs[i].Name[0] == '.')
+                if (ok == false && fs[i].Name[0] == '.')//если он скрытый и нам не нужно его отображать
                 {
-                    continue;
-                }
-                Color(fs[i], k);
-                Console.Write(k+1); Console.Write('.');
-                Console.WriteLine(fs[i].Name);
-                k++;
+                    continue;//продолжаем
+                }//иначе
+                Color(fs[i], k);//определяем цвет с помощью функции и индекса
+                Console.Write(k+1); Console.Write('.');//выводим нумерацию
+                Console.WriteLine(fs[i].Name);//и имя элемента
+                k++;//отдельный счетчик для нумерации
             }
         }
 
-        public void Start()
+        public void Start()//функция для запуска реагирует на нажатия пользователем клавиши
         {
             ConsoleKeyInfo CK = Console.ReadKey();
-            while (CK.Key != ConsoleKey.Escape)
+            while (CK.Key != ConsoleKey.Escape)//пока пользователь не выйдет из консоли
             {
-                CalcSz();
-                Show();
-                CK = Console.ReadKey();
+                CalcSz();//считаем размерность
+                Show();//отображаем список
+                CK = Console.ReadKey();//ждем команды
                 if (CK.Key == ConsoleKey.UpArrow) Up();
                 if (CK.Key == ConsoleKey.DownArrow) Down();
                 if (CK.Key == ConsoleKey.LeftArrow) Left();
                 if (CK.Key == ConsoleKey.RightArrow) Right();
-                if (CK.Key == ConsoleKey.Enter)
+                //при нажатии определенных клавиш вызываем соответствующие функции
+                if (CK.Key == ConsoleKey.Enter)//при нажатии Enter
                 {
-                    if (currentFs.GetType() == typeof(DirectoryInfo))
+                    if (currentFs.GetType() == typeof(DirectoryInfo))//если нажали на папку
                     {
                         cursor = 0;
-                        path = currentFs.FullName;
+                        path = currentFs.FullName;//программа начнет работать внутри этой папки
                     }
                     else
-                    {
+                    {//если это файл
                         Console.BackgroundColor = ConsoleColor.Black;
-                        Console.Clear();
-                        string path1 = currentFs.FullName;
+                        Console.Clear();//очищаем экран
+                        string path1 = currentFs.FullName;//определяем путь к текущему файлу
                         StreamReader sr = new StreamReader(path1);
-                        string s1 = sr.ReadToEnd();
+                        string s1 = sr.ReadToEnd();//считываем
                         sr.Close();
-                        Console.Write(s1);
-                        Console.ReadKey();
+                        Console.Write(s1);//выводим на него содержимое файла
+                        Console.ReadKey();//ждем нажатия клавиши
                     }
                 }
-                if (CK.Key == ConsoleKey.Backspace)
+                if (CK.Key == ConsoleKey.Backspace)//если нажали Backspace
                 {
                      cursor = 0;
-                     path = directory.Parent.FullName;
+                     path = directory.Parent.FullName;//перемещаемся в предыдущую папку
                 }
-                if (CK.Key == ConsoleKey.Delete)
+                if (CK.Key == ConsoleKey.Delete)//если нажали Delete
                 {
-                    string path1 = currentFs.FullName;
-                    if (currentFs.GetType() == typeof(FileInfo))
+                    string path1 = currentFs.FullName;//определяем путь к текущей папке/файлу
+                    if (currentFs.GetType() == typeof(FileInfo))//если это файл
                     {
-                        File.Delete(path1);
+                        File.Delete(path1);//удаляем
                     }
-                    if (currentFs.GetType() == typeof(DirectoryInfo))
+                    if (currentFs.GetType() == typeof(DirectoryInfo))//если папка
                     {
-                        Directory.Delete(path1);
+                        Directory.Delete(path1);//используем другую функцию
                     }
+                }
+                if (CK.Key == ConsoleKey.Tab)//если пользователь нажмет Tab, значит он хочет переименовать 
+                {
+                    string path1 = directory.FullName;//фиксируем путь к текущей папке
+                    Console.BackgroundColor = ConsoleColor.Black;
+                    Console.Clear();//очищаем консоль
+                    string name = Console.ReadLine();//вводим новое имя элемента
 
+                    if (currentFs.GetType() == typeof(FileInfo))//если это файл
+                    {
+                         string sourcefile = currentFs.FullName;//первоначальный файл мы
+                         string destfile = Path.Combine(path1, name);
+                         File.Move(sourcefile, destfile);//преобразуем во второй
+                    }
+                    else
+                    if (currentFs.GetType() == typeof(DirectoryInfo))//если папка
+                    {
+                        string sourcedir = currentFs.FullName;
+                        string destdir = Path.Combine(path1, name);//к ссылке папки добавляем имя элемента
+                        Directory.Move(sourcedir, destdir);
+                    }//получается, что мы на самом деле копируем старый элемент в новый, изменяя название
+                   
                 }
             }
         }
