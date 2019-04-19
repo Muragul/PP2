bf87c59937cce9198f7c16d557381c62d9550cb6
Muragul <muragulm@gmail.com>
2019-04-20 01:47:49 +0600

    prepared to quiz 3

--- Preparation/bounce/bounce/Form1.cs
+++ Preparation/bounce/bounce/Form1.cs
@@ -20,8 +20,10 @@ namespace bounce
             timer1.Enabled = true;
         }
 
-        int dx = 5, dy = 5;
-        int x = 50, y = 50, r = 20;
+        int dx = 10, dy = 10;
+        int x = 0, y = 0, r = 10;
+        double angle = Math.PI / 4;
+        float a, b;
 
         private void Form1_MouseDown(object sender, MouseEventArgs e)
         {
@@ -38,11 +40,26 @@ namespace bounce
 
         private void Form1_Paint(object sender, PaintEventArgs e)
         {
-            e.Graphics.FillEllipse(pen.Brush, x - r, y - r, 2 * r, 2 * r);
+            e.Graphics.FillEllipse(pen.Brush, a, b, 2 * r, 2 * r);
             x += dx;
             y += dy;
-            if (y+r+r >= this.Height || y <= 0) dy *= -1;
-            if (x+r+r >= this.Width || x <= 0) dx *= -1;
+            a = (float) (x * Math.Cos(angle));
+            b = (float) (x * Math.Sin(angle));
+
+            if (b+r+r+20 >= this.Height || y <= 0) dy *= -1;
+            if (a+r+r >= this.Width || x <= 0 ) dx *= -1;
         }
     }
 }
+//for simple bounce
+        //int dx = 5, dy = 5;
+        //int x = 50, y = 50, r = 20;
+
+        //private void Form1_Paint(object sender, PaintEventArgs e)
+        //{
+        //    e.Graphics.FillEllipse(pen.Brush, x - r, y - r, 2 * r, 2 * r);
+        //    x += dx;
+        //    y += dy;
+        //    if (y + r + r >= this.Height || y <= 0) dy *= -1;
+        //    if (x + r + r >= this.Width || x <= 0) dx *= -1;
+        //}
